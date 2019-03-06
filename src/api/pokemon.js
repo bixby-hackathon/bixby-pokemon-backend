86cc377b7126e0778b1984e945a9bbec943024df
Jon Pan <jonmpan@gmail.com>
2019-03-06 00:00:03 -0800

    added error response

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -7,37 +7,41 @@ export default ({ config }) => {
   let api = Router();
 
   api.get('/:name', async (req, res) => {
-    const pokemon = await P.getPokemonByName(req.params.name);
-    const formattedPokemon = {};
-    const speciesInfo = await P.getPokemonSpeciesByName(req.params.name);
+    try {
+      const pokemon = await P.getPokemonByName(req.params.name);
+      const formattedPokemon = {};
+      const speciesInfo = await P.getPokemonSpeciesByName(req.params.name);
 
-    formattedPokemon.id = pokemon.id;
-    formattedPokemon.sprite = pokemon.sprites.front_default;
-    formattedPokemon.pokedexEntry = speciesInfo.flavor_text_entries.find(
-      o => o.language.name === 'en',
-    ).flavor_text;
-    formattedPokemon.height = pokemon.height;
-    formattedPokemon.weight = pokemon.weight;
-    formattedPokemon.statHp = pokemon.stats.find(
-      o => o.stat.name === 'attack',
-    ).base_stat;
-    formattedPokemon.statAttack = pokemon.stats.find(
-      o => o.stat.name === 'attack',
-    ).base_stat;
-    formattedPokemon.statDefense = pokemon.stats.find(
-      o => o.stat.name === 'defense',
-    ).base_stat;
-    formattedPokemon.statSpecialAttack = pokemon.stats.find(
-      o => o.stat.name === 'special-attack',
-    ).base_stat;
-    formattedPokemon.statSpecialDefense = pokemon.stats.find(
-      o => o.stat.name === 'special-defense',
-    ).base_stat;
-    formattedPokemon.statSpeed = pokemon.stats.find(
-      o => o.stat.name === 'speed',
-    ).base_stat;
+      formattedPokemon.id = pokemon.id;
+      formattedPokemon.sprite = pokemon.sprites.front_default;
+      formattedPokemon.pokedexEntry = speciesInfo.flavor_text_entries.find(
+        o => o.language.name === 'en',
+      ).flavor_text;
+      formattedPokemon.height = pokemon.height;
+      formattedPokemon.weight = pokemon.weight;
+      formattedPokemon.statHp = pokemon.stats.find(
+        o => o.stat.name === 'attack',
+      ).base_stat;
+      formattedPokemon.statAttack = pokemon.stats.find(
+        o => o.stat.name === 'attack',
+      ).base_stat;
+      formattedPokemon.statDefense = pokemon.stats.find(
+        o => o.stat.name === 'defense',
+      ).base_stat;
+      formattedPokemon.statSpecialAttack = pokemon.stats.find(
+        o => o.stat.name === 'special-attack',
+      ).base_stat;
+      formattedPokemon.statSpecialDefense = pokemon.stats.find(
+        o => o.stat.name === 'special-defense',
+      ).base_stat;
+      formattedPokemon.statSpeed = pokemon.stats.find(
+        o => o.stat.name === 'speed',
+      ).base_stat;
 
-    res.status(200).json(formattedPokemon);
+      res.status(200).json(formattedPokemon);
+    } catch (error) {
+      res.status(404).json({ message: 'error' });
+    }
   });
 
   return api;
