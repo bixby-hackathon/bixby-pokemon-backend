b1a02bae4b1e09d2fd0a2a85768696153ba73c84
Jonathan Pan <jonmpan@gmail.com>
2019-03-07 18:54:59 -0800

    added new get route

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -136,5 +136,101 @@ export default ({ config }) => {
     }
   });
 
+  api.get('/more/:name', async (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    try {
+      let query = '';
+      if (req.params.name === 'meowstic') {
+        query = 'meowstic-male';
+      } else {
+        query = replaceSpaces(req.params.name);
+      }
+      let speciesInfo = {};
+      const pokemon = await P.getPokemonByName(query);
+      if (query === 'meowstic-male' || query === 'meowstic-female') {
+        speciesInfo = await P.getPokemonSpeciesByName('meowstic');
+      } else {
+        speciesInfo = await P.getPokemonSpeciesByName(query);
+      }
+      const formattedPokemon = {};
+      const abilities = pokemon.abilities;
+      const ability1 = abilities.find(o => o.slot === 1);
+      const ability2 = abilities.find(o => o.slot === 2);
+      const ability3 = abilities.find(o => o.slot === 3);
+      let ability1Resource = '';
+      let ability2Resource = '';
+      let ability3Resource = '';
+
+      if (ability1) {
+        ability1Resource = await P.resource(ability1.ability.url);
+        formattedPokemon.ability1Description = removeLinebreaks(
+          ability1Resource.effect_entries.find(o => o.language.name === 'en')
+            .short_effect,
+        );
+        formattedPokemon.ability1 = ability1.ability.name;
+      }
+      if (ability2) {
+        ability2Resource = await P.resource(ability2.ability.url);
+        formattedPokemon.ability2Description = removeLinebreaks(
+          ability2Resource.effect_entries.find(o => o.language.name === 'en')
+            .short_effect,
+        );
+        formattedPokemon.ability2 = ability2.ability.name;
+      }
+      if (ability3) {
+        ability3Resource = await P.resource(ability3.ability.url);
+        formattedPokemon.ability3Description = removeLinebreaks(
+          ability3Resource.effect_entries.find(o => o.language.name === 'en')
+            .short_effect,
+        );
+        formattedPokemon.ability3 = ability3.ability.name;
+      }
+
+      formattedPokemon.id = pokemon.id;
+      formattedPokemon.species = capitalizeFirstLetter(pokemon.name);
+      if (query === 'meowstic-male' || query === 'meowstic-female') {
+        formattedPokemon.species = 'Meowstic';
+      }
+      formattedPokemon.sprite = pokemon.sprites.front_default;
+      formattedPokemon.sprites = getSprites(pokemon.sprites);
+      if (pokemon.types.length > 1) {
+        formattedPokemon.type1 = pokemon.types[1].type.name;
+        formattedPokemon.type2 = pokemon.types[0].type.name;
+      } else {
+        formattedPokemon.type1 = pokemon.types[0].type.name;
+      }
+      formattedPokemon.pokedexEntry = removeLinebreaks(
+        speciesInfo.flavor_text_entries.find(o => o.language.name === 'en')
+          .flavor_text,
+      );
+      formattedPokemon.height = pokemon.height / 10;
+      formattedPokemon.weight = pokemon.weight / 10;
+      formattedPokemon.statHp = pokemon.stats.find(
+        o => o.stat.name === 'attack',
+      ).base_stat;
+      formattedPokemon.statAttack = pokemon.stats.find(
+        o => o.stat.name === 'attack',
+      ).base_stat;
+      formattedPokemon.statDefense = pokemon.stats.find(
+        o => o.stat.name === 'defense',
+      ).base_stat;
+      formattedPokemon.statSpecialAttack = pokemon.stats.find(
+        o => o.stat.name === 'special-attack',
+      ).base_stat;
+      formattedPokemon.statSpecialDefense = pokemon.stats.find(
+        o => o.stat.name === 'special-defense',
+      ).base_stat;
+      formattedPokemon.statSpeed = pokemon.stats.find(
+        o => o.stat.name === 'speed',
+      ).base_stat;
+
+      res.status(200).json(formattedPokemon);
+    } catch (error) {
+      res.status(404).json({ message: error.message });
+    }
+  });
+
   return api;
 };
